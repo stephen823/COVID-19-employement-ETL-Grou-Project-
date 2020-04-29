eb22085ad0bceea07d50199040c370e744703fc2
LatyrT <60825191+LatyrT@users.noreply.github.com>
2020-04-29 08:01:39 -0400

    added a query
    added a query

--- UNEMPLOYMENT_TABLES.sql
+++ UNEMPLOYMENT_TABLES.sql
@@ -93,3 +93,13 @@ SELECT * FROM testing_count;
 
 
 -----------
+SELECT *
+FROM CASES_COUNT AS C
+INNER JOIN UNEMPLOYMENT AS U
+ON C.DATE = U.REPORTING_DATE
+WHERE U.SEX <> 'Both sexes'
+AND U.CATEGORY = 'Percentage'
+;
+
+
+
